f46be6202f42126e11b1f0880bcb7dfe4dafb306
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2021-12-03 15:46:42 +0100

    Update sonarQube01.java

--- sonarQube01.java
+++ sonarQube01.java
@@ -1,5 +1,6 @@
+package main;
 
-public class sonarQube01 {
+public class SonarQube01 {
     String addHeader(String stringToAdd, int x, int y){
         return String.format("Added Header %s %d %d", stringToAdd, x, y);
     }
