a2579309322a46c84eded0d2dc1d43d2d9854c11
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2021-12-03 16:22:22 +0100

    Delete sonarQube02.java

--- sonarQube02.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class SonarQube02 {
-
-    String printNumbers(String stringToAdd, int x, int y) {
-        return String.format("Numbers are %s %d %d", stringToAdd, x, y);
-    }
-
-}
