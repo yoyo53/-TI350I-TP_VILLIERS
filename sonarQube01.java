986db89f632da8325339a371d88d97c3730a5110
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2021-12-03 16:33:21 +0100

    Delete sonarQube01.java

--- sonarQube01.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class SonarQube01 {
-    String addHeader(String stringToAdd, int x, int y){
-        return String.format("Added Header %s %d %d", stringToAdd, x, y);
-    }
-}
